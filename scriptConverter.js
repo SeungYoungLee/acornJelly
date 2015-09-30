4b860dc27c74dc67c0d3481585c9afd3d9dfd5c3
maninzoo <maninzoo@inswave.com>
2015-09-30 20:21:26 +0900

    Add fileManager

--- scriptConverter.js
+++ scriptConverter.js
@@ -1,37 +1,52 @@
-var acorn = require('acorn/dist/acorn_loose');
-var fs = require("fs");
+var fs = require('fs'),
+    acorn = require('acorn/dist/acorn_loose');
 
-var tokenize = false,
-    silent = false;
+var fileManager = require('./util/fileManager');
 
-var fileList = [
-    'samples/basic.js',
-    'samples/test.js'
-  ],
-  infile = fileList[1];
+var confFile = './conf/scriptConverter.json',
+    content = fs.readFileSync( confFile, 'utf-8' ),
+    conf = JSON.parse( content ),
+    srcPath = conf.srcPath,
+    destPath = conf.destPath;
 
-var parsed,
-    tokens,
-    options = {};
+for ( sp in srcPath ) {
+  console.log( srcPath[sp] );
 
-options.locations = true;
-
-if ( tokenize ) {
-  tokens = [];
-  options.onToken = tokens;
+  var hierarchy = fileManager.getFileHierarchy( srcPath[sp] );
+  console.log( JSON.stringify(hierarchy) );
 }
 
-var code = fs.readFileSync( infile, "utf8" );
-code = code.replace( /<>/g, '!=' ).replace( /Var\s+/g, 'var ' );
-
-try {
-  parsed = acorn.parse_dammit( code, options );
-} catch(e) {
-  // SyntaxError
-  console.error( e.message + ' loc ' + JSON.stringify( e.loc ) );
-  process.exit(1);
-}
-
-if ( !silent ) {
-  console.log( JSON.stringify( tokenize ? tokens : parsed, null, 2 ) );
-}
+//var tokenize = false,
+//    silent = false;
+//
+//var fileList = [
+//    'samples/basic.js',
+//    'samples/test.js'
+//  ],
+//  infile = fileList[1];
+//
+//var parsed,
+//    tokens,
+//    options = {};
+//
+//options.locations = true;
+//
+//if ( tokenize ) {
+//  tokens = [];
+//  options.onToken = tokens;
+//}
+//
+//var code = fs.readFileSync( infile, "utf8" );
+//code = code.replace( /<>/g, '!=' ).replace( /Var\s+/g, 'var ' );
+//
+//try {
+//  parsed = acorn.parse_dammit( code, options );
+//} catch(e) {
+//  // SyntaxError
+//  console.error( e.message + ' loc ' + JSON.stringify( e.loc ) );
+//  process.exit(1);
+//}
+//
+//if ( !silent ) {
+//  console.log( JSON.stringify( tokenize ? tokens : parsed, null, 2 ) );
+//}
---
+++ util/fileManager.js
@@ -0,0 +1,75 @@
+var fs = require('fs'),
+    path = require('path');
+
+var fileManager = require('./fileManager');
+
+var ArrayProto = Array.prototype,
+    push       = ArrayProto.push;
+
+module.exports.getFileList = function ( dir, done ) {
+  var results = [];
+  fs.readdir( dir, function( err, list ) {
+    if (err) {
+      return done(err);
+    }
+
+    var pending = list.length;
+    if ( !pending ) {
+      return done( null, results );
+    }
+
+    list.forEach( function(file) {
+      file = path.resolve( dir, file );
+      fs.stat( file, function( err, stat ) {
+        if ( stat && stat.isDirectory() ) {
+          fileManager.getFileList( file, function( err, res ) {
+            push.apply( results, res );
+            if ( !--pending ) done( null, results );
+          } );
+        } else {
+          // TODO option 로 제외하거나 제한할 목록을 받아서 filtering 한다.
+          if ( file.lastIndexOf( '.xml' ) > 1 ) {
+            results.push(file);
+          } else {
+            console.log( 'Omit ' + file );
+          }
+          if ( !--pending ) done( null, results );
+        }
+      } );
+    } );
+  } );
+};
+
+module.exports.getFileHierarchy = function ( dir, hierarchy, done ) {
+  if ( !hierarchy ) {
+    hierarchy = {};
+  }
+
+  hierarchy[dir] = {
+    files: [],
+    dirs: []
+  };
+
+  var fullPath,
+      extName = '',
+      list = fs.readdirSync(dir);
+
+  list.forEach( function(file) {
+    fullPath = path.resolve( dir, file );
+
+    if ( fs.statSync(fullPath).isDirectory() ) {
+      var subDir = {};
+      hierarchy[dir].dirs.push( subDir );
+      fileManager.getFileHierarchy( fullPath, subDir );
+    } else {
+      extName = path.extname(file);
+      if ( extName === '.xml' || extName === '.js' ) {
+        hierarchy[dir].files.push(file);
+      } else {
+        console.log( 'Invalid ext name. ' + fullPath );
+      }
+    }
+  } );
+
+  return hierarchy;
+};
